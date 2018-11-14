825dad50649462bee73761c8c6bd728b44c7b663
niffler-bkkkkk <874431603@qq.com>
2018-11-14 20:41:49 +0800

    fix: recruit-new响应式

--- src/components/recruit-new.vue
+++ src/components/recruit-new.vue
@@ -6,8 +6,18 @@
       <span class="title-text">我们欢迎</span>
     </div>
     <div class="part-content">
-      <div class="card-container card-container1">
-        <div class="content-card" v-for="(item,index) in cardInfo1" :key="index">
+      <div class="card-container">
+        <div class="content-card" v-for="(item) in cardInfo.slice(0,2)" :key="item.id">
+          <div class="card-card card1"></div>
+          <div class="card-card card2"></div> 
+          <div class="card-content">
+            <div class="content-title">{{item.title}}</div>
+            <div class="content-text">{{item.content}}</div>
+          </div>
+        </div>
+      </div>
+      <div class="card-container">
+        <div class="content-card" v-for="(item) in cardInfo.slice(2,4)" :key="item.id">
           <div class="card-card card1"></div>
           <div class="card-card card2"></div> 
           <div class="card-content">
@@ -19,8 +29,18 @@
     </div>
     <img class="part-image" src="../assets/Illustrations/design_process_iqqg.svg">
     <div class="part-content">
-      <div class="card-container card-container2">
-        <div class="content-card" v-for="(item,index) in cardInfo2" :key="index">
+      <div class="card-container">
+        <div class="content-card" v-for="(item) in cardInfo.slice(4,6)" :key="item.id">
+          <div class="card-card card1"></div>
+          <div class="card-card card2"></div>
+          <div class="card-content">
+            <div class="content-title">{{item.title}}</div>
+            <div class="content-text">{{item.content}}</div>
+          </div>
+        </div>
+      </div>
+      <div class="card-container">
+        <div class="content-card" v-for="(item) in cardInfo.slice(6)" :key="item.id">
           <div class="card-card card1"></div>
           <div class="card-card card2"></div>
           <div class="card-content">
@@ -38,48 +58,46 @@ export default {
   name: `Recruit`,
   data() {
     return {
-      cardInfo1: [
+      cardInfo: [
         {
-          title: "前端",
-          content:
-            "负责团队项目的前端开发，掌握HTML/CSS，基本了解Javascript，能与团队其他成员友好交流。"
+          id: 1,
+          title: '前端',
+          content: '负责团队项目的前端开发，掌握HTML/CSS，基本了解Javascript，能与团队其他成员友好交流。'
         },
         {
-          title: "后端",
-          content:
-            "负责团队项目的后端开发，掌握爬虫的开发技巧，掌握至少一门后端开发语言，积极向上。"
+          id: 2,
+          title: '后端',
+          content: '负责团队项目的后端开发，掌握爬虫的开发技巧，掌握至少一门后端开发语言，积极向上。'
         },
         {
-          title: "ios | android",
-          content:
-            "完成团队项目的多终端部署计划，掌握Object-C，Swift，Java中任意一门语言，有较强的创新精神。"
+          id: 3,
+          title: 'ios | android',
+          content: '完成团队项目的多终端部署计划，掌握Object-C，Swift，Java中任意一门语言，有较强的创新精神。'
         },
         {
-          title: "测试工程师",
-          content:
-            "对团队的产品进行发布前的100%覆盖测试，有一定的全栈知识，能够预估产品Bug产生的原因。"
-        }
-      ],
-      cardInfo2: [
-        {
-          title: "UI | UX",
-          content:
-            "负责web/移动端的界面设计，能与前端的童鞋友好交流，并且具有一定读文档的能力，积极分享设计经验。"
+          id: 4,
+          title: '测试工程师',
+          content: '对团队的产品进行发布前的100%覆盖测试，有一定的全栈知识，能够预估产品Bug产生的原因。'
         },
         {
-          title: "画师",
-          content:
-            "负责web/移动端的界面设计，能与前端的童鞋友好交流，并且具有一定读文档的能力，积极分享设计经验。"
+          id: 5,
+          title: 'UI | UX',
+          content: '负责web/移动端的界面设计，能与前端的童鞋友好交流，并且具有一定读文档的能力，积极分享设计经验。'
         },
         {
-          title: "运营",
-          content:
-            "掌握新媒体运营的相关技术，对互联网具有较强的敏感性，擅长策划宣传工作，熟练使用平面设计相关软件。"
+          id: 6,
+          title: '画师',
+          content: '负责web/移动端的界面设计，能与前端的童鞋友好交流，并且具有一定读文档的能力，积极分享设计经验。'
+        },
+        {
+          id: 7,
+          title: '运营',
+          content: '掌握新媒体运营的相关技术，对互联网具有较强的敏感性，擅长策划宣传工作，熟练使用平面设计相关软件。'
         }
       ]
-    };
+    }
   }
-};
+}
 </script>
 
 
@@ -102,6 +120,7 @@ export default {
     margin-top: 2rem
     @media(max-width: $phone)
       height: 15rem
+      background-size: 100%
     .title-text
       font-size: 2.5rem
       color: #222222
@@ -115,13 +134,15 @@ export default {
     background-size: 100%
     .card-container
       margin: 0 auto
-      // height: 60rem
       display: flex
-      flex-wrap: wrap
       flex-direction: row
+      flex-wrap: wrap
       justify-content: space-between
+      align-items: center
       @media(min-width: $phone)
         width: 72%
+      @media(max-width: $ipad)
+        justify-content: center
       .content-card
         position: relative
         height: 25rem
@@ -129,22 +150,25 @@ export default {
         display: flex
         align-items: center
         justify-content: center
-        @media(min-width: $phone)
-          margin: 2rem
+        margin: 2rem
+        @media(max-width: $phone)
+          height: 18rem
+          width: 18rem
+          margin: 0 auto
         .card-card
           background: #dcfbb5
           width: 20rem
           height: 20rem
           opacity: 0.5
           position: absolute
-          transition: all .2s ease .2s
+          transition: all .2s ease .0s
           @media(max-width: $phone)
-            width: 16rem
-            height: 16rem
+            height: 14rem
+            width: 14rem
         .card1
           transform: rotate(10deg)
         .card2
-          transform: rotate(-100deg)
+          transform: rotate(-60deg)
         .card-content
           height: 8rem
           width: 16rem
@@ -153,19 +177,26 @@ export default {
           align-items: center
           flex-direction: column
           text-align: justify
+          @media(max-width: $phone)
+            height: 5rem
+            width: 10rem
           .content-title
             font-size: 1.4rem
             font-weight: 500
             color: #22b574
             z-index: 1
+            @media(max-width: $phone)
+              font-size: 1.2rem
           .content-text
             margin-top: 1rem
             color: #222222
             z-index: 1
+            @media(max-width: $phone)
+              font-size: 0.8rem
         &:hover
           .card1,.card2
             transform: rotate(0)
-            transition: all .2s ease-in-out .2s
+            transition: all .2s ease .0s
   .part-image
     width: 72%
     margin: 0 0
